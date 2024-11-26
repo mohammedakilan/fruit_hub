896d6e3104613859d64b8d8dc38718d264e9099c
Mohammed Akilan <120200261@std.up.edu.ps>
2024-11-26 14:40:39 +0200

    splash view logic done

--- lib/core/helpersFunction/on_generate_route.dart
+++ lib/core/helpersFunction/on_generate_route.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
-    case SplashView.routeName:
-      return MaterialPageRoute(
-        builder: (context) => SplashView(),
+    case SplashView.routeName: return MaterialPageRoute(
+        builder: (context) => const SplashView(),
       );
-    default:
-      return MaterialPageRoute(
-        builder: (context) => const Scaffold(
-          body: Center(child: Text('Page not found')),
-        ),
+      
+    case OnBoardingView.routeName: return MaterialPageRoute(
+        builder: (context) => const OnBoardingView(),
+      );
+    default: return MaterialPageRoute(
+        builder: (context) => const Scaffold( ),
       );
   }
+ 
 }
