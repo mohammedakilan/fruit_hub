ce52ed13648e2da8da6878778f76784b16ba46f5
Mohammed Akilan <120200261@std.up.edu.ps>
2024-12-01 13:41:31 +0200

    add error handling with Failure abstraction and update auth repository

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hub/core/errors/failure.dart';
 import 'package:fruit_hub/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword({
+  Future<Either< Failure , UserEntity>> createUserWithEmailAndPassword({
     required String email,
     required String password,
     required String name,
